1cf4a6d569a08ce65b7cd73099338d01b0a357fb
EINDEX <eindex.me@outlook.com>
2017-07-30 01:11:53 +0800

    next-greater-element-i python solution

--- leetcode/496-next-greater-element-i/next-greater-element-i.python.py
+++ leetcode/496-next-greater-element-i/next-greater-element-i.python.py
@@ -9,7 +9,9 @@ class Solution(object):
         def b():
             x = nums.index(n)
             for c in range(x,len(nums)-1):
+                print c
                 a = nums[c+1]
+                x+=1
                 if a > n:
                     return a
             else:
