ee81e2675552da8b0842e076ff8ad9e3b5d8859d
EINDEX <eindex.me@outlook.com>
2019-03-19 00:42:06 +0800

    subsets python solution

--- leetcode/78-subsets/subsets.python3.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    # Iteratively
-    def subsets(self, nums):
-        res = [[]]
-        for num in sorted(nums):
-            res += [item+[num] for item in res]
-        return res
