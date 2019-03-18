0897b29ce7244a6f87eaa419078f811e7468bbf7
EINDEX <eindex.me@outlook.com>
2019-03-19 00:45:50 +0800

    peak-index-in-a-mountain-array python solution

--- leetcode/882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.python3.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def peakIndexInMountainArray(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        for i in range(len(A)):
-            if A[i] > A[i+1]:
-                return i
