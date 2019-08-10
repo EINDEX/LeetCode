a3ffd28effd8e964ff8bcbfc9d7d1c3996ce6706
EINDEX <eindex.me@outlook.com>
2019-08-10 18:31:15 +0800

    print-in-order python3 solution

---
+++ leetcode/1203-print-in-order/print-in-order.python3.py
@@ -0,0 +1,30 @@
+import time
+class Foo:
+    def __init__(self):
+        self.status = 0
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        while self.status != 0:
+            time.sleep(0.001)
+        printFirst()
+        self.status = 1
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        while self.status != 1:
+            time.sleep(0.001)
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.status = 2
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        while self.status != 2:
+            time.sleep(0.001)
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
